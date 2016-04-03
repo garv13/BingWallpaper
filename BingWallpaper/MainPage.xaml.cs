dbf685e7e260d883b2a0af3a6ace7a8570a2fe1f
garv13 <garvjasuja@outlook.com>
2016-04-03 18:19:33 +0530

    just basic structure
    not working

---
+++ BingWallpaper/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media;
+using Windows.Storage;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BingWallpaper
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient t = new HttpClient();
+            RootObject i = new RootObject();
+            string res = await t.GetStringAsync("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1");
+            i = JsonConvert.DeserializeObject<RootObject>(res);
+            string str = "http://bing.com" + i.images[0].url;
+            BitmapImage img = new BitmapImage(new Uri(str));
+            WriteableBitmap wb = new WriteableBitmap(img.PixelWidth, img.PixelHeight);
+
+            WriteableBitmap writeableBmp = BitmapFactory.New()
+            
+            imageX.Source = img;
+            await SaveBitmapToFileAsync(wb, i.images[0].copyright);
+        }
+        public static async Task SaveBitmapToFileAsync(WriteableBitmap image, string userId)
+        {
+            StorageFolder pictureFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync("ProfilePictures", CreationCollisionOption.OpenIfExists);
+            var file = await pictureFolder.CreateFileAsync(userId + ".jpg", CreationCollisionOption.ReplaceExisting);
+
+            using (var stream = await file.OpenStreamForWriteAsync())
+            {
+                BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream.AsRandomAccessStream());
+                var pixelStream = image.PixelBuffer.AsStream();
+                byte[] pixels = new byte[image.PixelBuffer.Length];
+
+                await pixelStream.ReadAsync(pixels, 0, pixels.Length);
+
+                encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore, (uint)image.PixelWidth, (uint)image.PixelHeight, 96, 96, pixels);
+
+                await encoder.FlushAsync();
+            }
+        }
+    }
+}
